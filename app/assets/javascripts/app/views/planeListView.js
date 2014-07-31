6bf8e88224ee7dbbf45d3c25d2d600480ec22a08
Rosalind Skinner <ros.skinner@gmail.com>
2014-07-31 12:49:19 +1000

    backbone plane models, views, colletions

--- app/assets/javascripts/app/views/planeListView.js
+++ app/assets/javascripts/app/views/planeListView.js
@@ -1,6 +1,6 @@
 var app = app || {};
 
-app.PostListView = Backbone.View.extend({
+app.PlaneListView = Backbone.View.extend({
   tagName: 'li',
   events: {
     'click': 'view'
@@ -10,13 +10,13 @@ app.PostListView = Backbone.View.extend({
   },
   render: function () {
     // THIS MAKES NO FRIGGIN SENSE!!!!!! WTF
-    var list_html = Handlebars.compile(app.templates.postListView)
+    var list_html = Handlebars.compile(app.templates.planeListView)
     var copy = list_html( this.model.toJSON() );
 
     this.$el.html( copy );
     return this.el;
   },
   view: function () {
-    app.router.navigate('posts/' + this.model.get('id'), true);
+    app.router.navigate('planes/' + this.model.get('id'), true);
   }
 });
--- app/assets/javascripts/app/views/planeView.js
+++ app/assets/javascripts/app/views/planeView.js
@@ -1,12 +1,12 @@
 var app = app || {};
 
-app.PostView = Backbone.View.extend({
+app.PlaneView = Backbone.View.extend({
   el: '#main',
   initialize: function () {
     this.render();
   },
   render: function () {
-    var post_html = Handlebars.compile(app.templates.postView);
+    var plane_html = Handlebars.compile(app.templates.planeView);
     var copy = post_html( this.model.toJSON() );
 
     this.$el.html( copy );
