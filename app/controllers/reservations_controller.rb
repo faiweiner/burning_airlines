8def2fa707d474b67e859a32899e635ed50ce3e5
TooDelirious <richfield14@gmail.com>
2014-07-31 14:03:43 +1000

    reservations controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -8,16 +8,30 @@ class ReservationsController < ApplicationController
     end
   end
 
-  def create
+  def show
+    @reservation = Reservations.find params[:id]
   end
 
-  def show
+  def create
+    @reservation = Reservation.create reservation_params
+    render :json => @reservation
   end
 
   def update
+    @reservation = Reservation.find params[:id]
+    @reservation.update reservation_params
+    render :json => @reservation
   end
 
   def destroy
+    @reservation = Reservation.find params[:id]
+    @reservation.destroy
+    root_to reservation_path # ROUTE TO BE CHANGED TO CORRECT PATH
+  end
+
+  private
+  def reservation_params
+    params.require(:id => params[:id], :user_id => params(:user_id), :date => params(:date), :flight_id => params(:flight_id) )
   end
 
 end
