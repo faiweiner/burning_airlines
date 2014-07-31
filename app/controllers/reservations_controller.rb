7a837b6920b0908db0dab3283f9afa341bf9b7ef
TooDelirious <richfield14@gmail.com>
2014-07-31 13:44:19 +1000

    updated controllers

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,23 @@
 class ReservationsController < ApplicationController
+
+  def index
+    @reservations = Reservation.all
+    respond_to do |format|
+      format.html {}
+      format.json{ render :json => @reservations}
+    end
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
