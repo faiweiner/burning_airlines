e32a431b7aa7339e2989116c9d181eb749600419
TooDelirious <richfield14@gmail.com>
2014-07-31 17:11:19 +1000

    add plane template

--- app/assets/javascripts/plane.js
+++ app/assets/javascripts/plane.js
@@ -5,7 +5,9 @@ $(document).ready(function(){
     appView: $('#plane-app-template').html(),
     planeListView: $('#plane-list-template').html(),
     planeView: $('#plane-template').html(),
+    planeAddView: $('#plane-add-template').html(),
   };
+
 
   app.router = new app.Router();
 
