d2cda557ea63bdc1011dfc8cf86ae727f4a192a2
Rosalind Skinner <ros.skinner@gmail.com>
2014-07-31 15:07:08 +1000

    trying to print out rows and aisles for planes

--- app/assets/javascripts/app/views/planeView.js
+++ app/assets/javascripts/app/views/planeView.js
@@ -8,6 +8,12 @@ app.PlaneView = Backbone.View.extend({
   render: function () {
     var plane_html = Handlebars.compile(app.templates.planeView);
     var copy = plane_html( this.model.toJSON() );
+    for(var r = 0; r <= this.model.get('rows'); r++) {
+      for (var a = 0; a <= this.model.get('aisles'); a++) {
+      console.log(r, a);
+      $('#seating-plan').append('<div/>');
+      }
+    }
 
     this.$el.html( copy );
   }
